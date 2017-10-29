bf0f86e7c795c83b1cc47e899220c54acebc9fcf
Man Hong Lee <manhong323@gmail.com>
2017-10-29 03:59:23 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,49 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combo_array|
+    first_index = combo_array[0]
+    second_index = combo_array[1]
+    third_index = combo_array[2]
+
+    arr = [board[first_index], board[second_index], board[third_index]]
+    all_x = arr.all? do |value|
+      value == "X"
+    end
+    all_o = arr.all? do |value|
+      value == "O"
+    end
+
+    if all_x || all_o
+      return combo_array
+    end
+  end
+  return false
+end
+
+def full?(board)
+  board.each do |value|
+    full = value.all? do |letter|
+      letter == "X" || letter == "O"
+    end
+    if full
+      true
+    end
+  end
+  false
+end
+
+board = ["", "", "", "O", "O", "O", " ", " ", " "]
+puts full?(board)
